0a108a5c0ed98823db1760158fa7f66e66cb6967
RoA10 <o223t029@st.kyoto-seika.ac.jp>
2025-07-07 15:24:29 +0900

    Update schema_dump.sql

--- schema_dump.sql
+++ schema_dump.sql
@@ -1,16 +1,1 @@
--- ユーザー情報
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username TEXT NOT NULL UNIQUE,
-  password_hash TEXT NOT NULL
-);
 
--- 授業情報
-CREATE TABLE classes (
-  class_id SERIAL PRIMARY KEY,
-  class_title TEXT NOT NULL,
-  required BOOLEAN NOT NULL DEFAULT false,
-  count INTEGER NOT NULL DEFAULT 0,
-  user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
