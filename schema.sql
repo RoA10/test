ed655c714fa331df36e9d40feb70c152972d3e49
RoA10 <o223t029@st.kyoto-seika.ac.jp>
2025-07-07 15:24:11 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,16 +1,1 @@
--- ユーザー情報
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
-);
 
--- 授業情報
-CREATE TABLE classes (
-    class_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    class_title TEXT NOT NULL,
-    required BOOLEAN NOT NULL DEFAULT 0,
-    count INTEGER NOT NULL DEFAULT 0,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
